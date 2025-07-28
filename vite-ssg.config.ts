a4dd96ff9ec1bc10bfaff40bdd32deaec58ba0a3
Prieul-Simon <simon.prieul@gmail.com>
2025-07-28 19:08:47 +0200

    Remove manifest from dist

--- vite-ssg.config.ts
+++ vite-ssg.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig, mergeConfig } from 'vite'
 import inherited from './vite.config'
+import { rm } from 'fs/promises'
 
 export default mergeConfig(
     inherited,
     defineConfig({
         build: {
             outDir: 'dist-ssg',
+        },
+        ssgOptions: {
+            formatting: 'none',
+            onFinished: async () => {
+                await rm('./dist-ssg/.vite', { recursive: true, })
+            },
         },
     })
 )
